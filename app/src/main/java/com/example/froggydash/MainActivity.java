a5f2ce90a49638ac116602834b9b456e828a8614
Leonid Romanovsky <sirLeonidRomanovsky@gmail.com>
2021-05-31 00:45:22 +0300

    added graphs and purged some shit

--- app/src/main/java/com/example/froggydash/MainActivity.java
+++ app/src/main/java/com/example/froggydash/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -21,9 +22,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.lang.ref.Reference;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
@@ -48,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
         auth.addAuthStateListener(this::onAuthStateChanged);
-
-        value("/public", this::log);
     }
 
     // callbacks
@@ -66,29 +74,57 @@ public class MainActivity extends AppCompatActivity {
         else {
             log("Auth state changed: logged in " + user.getUid());
             loginLogoutButton.setText("Log out");
-            subscribe("/users/" + user.getUid(), dbUser -> {
+            cleanupOnLogout = subscribe("/users/" + user.getUid(), dbUser -> {
                 log("loaded user", user.getUid());
                 LinearLayout userView = spawnButton("User " + user.getUid(), treeRootLayout);
-                subscribeChilds(dbUser, "multifrogs", multifrog -> {
-                    log("loaded multifrog", multifrog.getKey());
-                    LinearLayout miltiView = spawnButton("Multifrog " + multifrog.getKey(), userView);
-                    subscribeChilds(multifrog, "frogs", frog -> {
-                        log("loaded frog", frog.getKey());
-                        LinearLayout frogView = spawnButton("Frog " + frog.getKey(), miltiView);
-                        subscribeChilds(frog, "sensors", sensor -> {
-                            log("loaded sensor", sensor.getKey());
-                            spawnButton("Sensor " + sensor.getKey(), frogView);
-                        });
-                        subscribeChilds(frog, "sensors", "readings", readings -> {
-                            int count = ((Map<String,String>)readings.getValue()).size();
-                            log("loaded " + count + " readings for", readings.getKey());
-                        });
-                    });
-                });
+                return chain(
+                    () -> treeRootLayout.removeView((View)userView.getParent()),
+                    subscribeChilds(dbUser, "multifrogs", multifrog -> {
+                        log("loaded multifrog", multifrog.getKey());
+                        LinearLayout miltiView = spawnButton("Multifrog " + multifrog.getKey(), userView);
+                        return chain(
+                            () -> userView.removeView((View)miltiView.getParent()),
+                            subscribeChilds(multifrog, "frogs", frog -> {
+                                log("loaded frog", frog.getKey());
+                                LinearLayout frogView = spawnButton("Frog " + frog.getKey(), miltiView);
+                                Map<String,Object> sensorsMap = (Map<String,Object>)frog.child("sensors").getValue();
+                                Map<String,LinearLayout> sensorViewsMap = new HashMap<>();
+                                sensorsMap.keySet().forEach(uuid -> sensorViewsMap.put(uuid,
+                                    spawnButton("Sensor " + uuid, frogView, true)));
+                                return chain(
+                                    () -> miltiView.removeView((View)frogView.getParent()),
+                                    subscribeChilds(frog, "sensors", sensor -> {
+                                        log("loaded sensor", sensor.getKey());
+                                        TextView label = new TextView(this);
+                                        label.setText(sensor.getValue().toString());
+                                        sensorViewsMap.get(sensor.getKey()).addView(label, 0);
+                                        return () -> sensorViewsMap.get(sensor.getKey()).removeView(label);
+                                    }),
+                                    subscribeChilds(frog, "sensors", "readings", readings -> {
+                                        Map<String,Long> readingsMap = (Map<String,Long>)readings.getValue();
+                                        ArrayList<Long> readingValues = new ArrayList<>(new TreeMap<>(readingsMap).values());
+                                        int count = readingValues.size();
+                                        log("loaded " + count + " readings for", readings.getKey());
+
+                                        int drawStart = Math.max(count - 9, 0);
+                                        int drawCount = count - drawStart;
+                                        DataPoint[] points = new DataPoint[drawCount];
+                                        for (int i = 0; i < drawCount; i++)
+                                            points[i] = new DataPoint(i, readingValues.get(drawStart + i));
+                                        GraphView graph = new GraphView(this);
+                                        graph.setVisibility(View.VISIBLE);
+                                        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
+                                        graph.addSeries(series);
+                                        graph.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 500));
+                                        sensorViewsMap.get(readings.getKey()).addView(graph);
+                                        return () -> sensorViewsMap.get(readings.getKey()).removeView(graph);
+                                    })
+                                );
+                            })
+                        );
+                    })
+                );
             });
-
-
-
         }
     }
 
@@ -107,22 +143,18 @@ public class MainActivity extends AppCompatActivity {
         textView.append("\n");
     }
 
-    void log(Object arg) {
-        textView.append("> ");
-        textView.append(arg == null ? "null" : arg.toString());
-        textView.append("\n");
-    }
-
-    LinearLayout spawnButton(String text, LinearLayout root) {
+    LinearLayout spawnButton(String text, LinearLayout root, boolean hidden) {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout children = new LinearLayout(this);
         children.setPadding(50, 0,0,0);
         children.setOrientation(LinearLayout.VERTICAL);
+        children.setVisibility(hidden ? View.GONE : View.VISIBLE);
 
         Button button = new Button(this);
         button.setText(text);
+        button.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
         button.setOnClickListener(view -> {
             if (children.getVisibility() == View.VISIBLE)
                 children.setVisibility(View.GONE);
@@ -134,6 +166,10 @@ public class MainActivity extends AppCompatActivity {
         layout.addView(children);
         root.addView(layout);
         return children;
+    }
+
+    LinearLayout spawnButton(String text, LinearLayout root) {
+        return spawnButton(text, root, false);
     }
 
     void requestEmailAndPassword(BiConsumer<String,String> callback) {
@@ -168,35 +204,20 @@ public class MainActivity extends AppCompatActivity {
 
     // Firebase helper functions
 
-    Runnable value(String path, Consumer<DataSnapshot> callback) {
-        //log("loading", path, "from db...");
-        DatabaseReference ref = database.getReference(path);
-        ValueEventListener listener =new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snap) {
-                //log("loaded", path);
-                callback.accept(snap);
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                //log("error loading:", path, error.toString());
-            }
-        };
-        ref.addListenerForSingleValueEvent(listener);
-        return () -> {
-            log("canceled loading", path, "from db...");
-            ref.removeEventListener(listener);
-        };
+    Runnable chain(Runnable ...runnables) {
+        return () -> { for (Runnable runnable : runnables) runnable.run(); };
     }
 
-    Runnable subscribe(String path, Consumer<DataSnapshot> callback) {
+    Runnable subscribe(String path, Function<DataSnapshot, Runnable> callback) {
         //log("subscribed to", path, "from db...");
         DatabaseReference ref = database.getReference(path);
+        AtomicReference<Runnable> uneffect = new AtomicReference<Runnable>();
         ValueEventListener listener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snap) {
                 //log("updated", path);
-                callback.accept(snap);
+                if (uneffect.get() != null) uneffect.get().run();
+                uneffect.set(callback.apply(snap));
             }
             @Override
             public void onCancelled(DatabaseError error) {
@@ -207,39 +228,21 @@ public class MainActivity extends AppCompatActivity {
         return () -> {
             log("unsubscribed from", path, "from db...");
             ref.removeEventListener(listener);
+            if (uneffect.get() != null) uneffect.get().run();
+            uneffect.set(null);
         };
     }
 
-    Map<String, String> asMap(Object obj, String ...path) {
-        Map<String, Object> ret = (Map<String, Object>)obj;
-        for (String name : path)
-            ret = (Map<String, Object>)ret.get(name);
-        return (Map<String, String>)(Object)ret;
-    }
-
-    Runnable valueChilds(DataSnapshot parent, String path, Consumer<DataSnapshot> callback) {
-        return valueChilds(parent,path,path,callback);
-    }
-
-    Runnable subscribeChilds(DataSnapshot parent, String path, Consumer<DataSnapshot> callback) {
+    Runnable subscribeChilds(DataSnapshot parent, String path, Function<DataSnapshot, Runnable> callback) {
         return subscribeChilds(parent,path,path,callback);
     }
 
-    Runnable valueChilds(DataSnapshot parent, String path, String rootPath, Consumer<DataSnapshot> callback) {
-        Map<String, String> map = asMap(parent.getValue(), path);
-        Runnable[] unsubscribers = new Runnable[map.size()];
+    Runnable subscribeChilds(DataSnapshot parent, String path, String rootPath, Function<DataSnapshot, Runnable> callback) {
+        Map<String, String> map = (Map<String, String>)parent.child(path).getValue();
+        Runnable[] unsubs = new Runnable[map.size()];
         int i = 0;
         for (String uuid: map.keySet())
-            unsubscribers[i++] = value("/" + rootPath + "/" + uuid, callback);
-        return () -> { for (Runnable unsubber : unsubscribers) unsubber.run(); };
-    }
-
-    Runnable subscribeChilds(DataSnapshot parent, String path, String rootPath, Consumer<DataSnapshot> callback) {
-        Map<String, String> map = asMap(parent.getValue(), path);
-        Runnable[] unsubscribers = new Runnable[map.size()];
-        int i = 0;
-        for (String uuid: map.keySet())
-            unsubscribers[i++] = subscribe("/" + rootPath + "/" + uuid, callback);
-        return () -> { for (Runnable unsubber : unsubscribers) unsubber.run(); };
+            unsubs[i++] = subscribe("/" + rootPath + "/" + uuid, callback);
+        return chain(unsubs);
     }
 }
